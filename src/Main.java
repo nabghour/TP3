0437c2081d6bb7a60f23d6dbc6f41662b2941833
Noreddine Abghour <nabghour@gmail.com>
2023-06-08 21:02:20 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-public class Main{
+public class Essai {
   public static void main(String[] args) 
   { 
-    System.out.println(“Hello,World”);
+    System.out.println("Bonjour tout le monde");
   }
 }
