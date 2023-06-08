b34472bade49cf7eb432f5a77cdbbf17e5ccc7d4
Noreddine Abghour <nabghour@gmail.com>
2023-06-08 21:58:44 +0100

    Update Essai.java

--- src/Essai.java
+++ src/Essai.java
@@ -1,6 +1,12 @@
 public class Essai {
   public static void main(String[] args) 
   { 
+    for(;;)
+    {
     System.out.println("*************************Bonjour LP FSDEVOPS****************************");
+    System.out.println("*************************Bonjour LP FSDEVOPS****************************");
+    System.out.println("*************************Bonjour LP FSDEVOPS****************************");
+    System.out.println("*************************Bonjour LP FSDEVOPS****************************");
+   } //fin for 
   }
 }
